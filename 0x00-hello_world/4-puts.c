6fb9041b90feb828665c3b8747e247818f0a50d2
lokky19 <thankgod.agodi@gmail.com>
2022-06-02 15:12:01 +0100

    Printig an output with PUT

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #!/bin/bash
 #include <stanio.h>
-int main ()
+int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
-
-    getchar();
-    return(0)
+puts("Programming is like building a multilingual puzzle");
+getchar();
+return(0);
 }
