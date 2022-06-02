930739ad2165e7e9d653393520f2c24ee62f3210
lokky19 <thankgod.agodi@gmail.com>
2022-06-02 15:47:47 +0100

    Hello world using C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
-#!/bin/bash
-#include <stanio.h>
+#include <stdio.h>
+
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
-getchar();
-return(0);
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
+
