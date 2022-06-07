f1aaeae3ccdb6de79c1182474661a0e81e6a7f02
lokky <thankgod.agodi@gmail.com>
2022-06-08 12:15:02 +1300

    Lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/*
+ * print_alphabet - prints lowercase alphabet, followed by new line.
+ *
+ * Description: Lowercase alphabet
+ *
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/*
+ * File: main.h
+ * Auth: Agodi ThankGod
+ * Description: Header file containing declarations of all
+ * the functions used in the 0x02-functions_nested_loops directory
+ *
+ */
+
 int _putchar(char c);
+void print_alphabet(void);
 
 #endif /* MAIN_H */
