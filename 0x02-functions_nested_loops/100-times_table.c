b8460bdf177c363bb337326cf0b4b7850a2dc656
lokky <thankgod.agodi@gmail.com>
2022-06-09 00:56:03 +1300

    n times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * print_times_table - The n times table.
+ *
+ * @n: An integer
+ *
+ * Return: Number matrix
+ */
+void print_times_table(int n)
+{
+	int num, mult, res;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (num = 0; num <= n; num++)
+		{
+			_putchar('0');
+
+			for (mult = 1; mult <= n; mult++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				res = num * mult;
+
+				if (res <= 99)
+					_putchar(' ');
+				if (res <= 9)
+					_putchar(' ');
+
+				if (res >= 100)
+				{
+					_putchar((res / 100) + '0');
+					_putchar((res / 10) % 10 + '0');
+				}
+				else if (res <= 99 && res >= 10)
+				{
+					_putchar((res / 10) + '0');
+				}
+				_putchar((res % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
