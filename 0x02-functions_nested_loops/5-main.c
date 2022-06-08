8113af9699a2203a39c2b19fceb7b7e18d89c267
lokky <thankgod.agodi@gmail.com>
2022-06-08 15:10:20 +1300

    Sings

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 
 /**
  * main - check the code.
@@ -8,6 +8,7 @@ nclude "main.h"
 int main(void)
 {
 	int r;
+
 	r = print_sign(98);
 	_putchar(',');
 	_putchar(' ');
