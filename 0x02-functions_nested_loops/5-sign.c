90a18ede83d6665a108938ba86c38b22b8b438b0
lokky <thankgod.agodi@gmail.com>
2022-06-08 20:17:45 +1300

    Sings

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,7 +12,7 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		_putchar('0');
 		return (1);
 	}
 	else if (n == 0)
@@ -22,7 +22,7 @@ int print_sign(int n)
 	}
 	else
 	{
-		_putchar(-1);
+		_putchar('0');
 		return (-1);
 	}
 }
